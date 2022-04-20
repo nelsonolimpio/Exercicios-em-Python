dab58c6365f12e743af03b4928a324e3fdba6899
Nelson Waiss Urenha Olimpio <nelsonolimpio@outlook.com>
2022-04-20 01:56:27 -0300

    Conversor de Metros em cm e mm.
    "Pedir uma medida em metros para o usuário, e imprimir quantos centímetros e quantos milímetros tem aquela medida digitada"

--- ex008.py
+++ ex008.py
@@ -1,3 +1,5 @@
+"Pedir uma medida em metros para o usuário, e imprimir quantos centímetros e quantos milímetros tem aquela medida digitada"
+
 m = float(input("Digite uma medida em metros:"))
 
 print("Metros {} é igual a {} cm e {} mm".format(m, (m*100), (m*1000)))

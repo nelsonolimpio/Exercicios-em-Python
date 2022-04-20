45174f992ef71344352468d1083d21821e2b8b59
Nelson Waiss Urenha Olimpio <nelsonolimpio@outlook.com>
2022-04-20 01:54:11 -0300

    Equações básicas com base no numero digitado.
    "Pedir um valor para o usuário. E imprimir o dobro do valor digitado, o triplo do valor digitado e a raiz quadrada do valor digitado"

--- ex006.py
+++ ex006.py
@@ -1,3 +1,5 @@
+"Pedir um valor para o usuário. E imprimir o dobro do valor digitado, o triplo do valor digitado e a raiz quadrada do valor digitado"
+
 num = int(input('Digite um número:'))
 
 print('Valor digitado = {}, Dobro do valor digitado= {}, Triplo do valor digitado = {} e Raiz quadrada do valor digitado = {}'.format(num, (num*2), (num*3), (num**(1/2))))

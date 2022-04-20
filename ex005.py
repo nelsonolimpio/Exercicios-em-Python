abdf3f0b521e4b1e658d744405fb2e0bc707896d
Nelson Waiss Urenha Olimpio <nelsonolimpio@outlook.com>
2022-04-20 01:52:16 -0300

    Valor anterior e posterior daquele que foi digit.
    "Usuário digita um número, e vamos imprimir o numero anterior e o posterior daquele que foi digitado pelo usuário."

--- ex005.py
+++ ex005.py
@@ -1,3 +1,5 @@
+"Usuário digita um número, e vamos imprimir o numero anterior e o posterior daquele que foi digitado pelo usuário."
+
 n = int(input('Digite um número:'))
 
 print("{},{},{}".format((n-1), n, (n+1)))

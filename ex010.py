a755142820826c4e7b0a1ef3e151a34b717ffb80
Nelson Waiss Urenha Olimpio <nelsonolimpio@outlook.com>
2022-04-20 01:59:18 -0300

    Conversos de R$ em $
    #Crie um programa que leia quanto de dinheiro ela tem na carteira e mostre quanto em dólar ela pode comprar.

--- ex010.py
+++ ex010.py
@@ -1,4 +1,4 @@
-#Crie um programa que leia quanto de dinheiro ela tem na carteira e mostre quanto em dólar ela pode comprar.
+"Crie um programa que leia quanto de dinheiro ela tem na carteira e mostre quanto em dólar ela pode comprar."
 
 valor = float(input("Digite o valor em reais:R$"))
 

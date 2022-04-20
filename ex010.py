09d1aad1761c60a0661cbf8c5f6a81558fc10ecf
Nelson Waiss Urenha Olimpio <nelsonolimpio@outlook.com>
2022-04-20 01:44:08 -0300

    Update ex010.py

--- ex010.py
+++ ex010.py
@@ -1,5 +1,5 @@
 #Crie um programa que leia quanto de dinheiro ela tem na carteira e mostre quanto em dólar ela pode comprar.
 
-valor = float(input("Dum valor em reais:R$"))
+valor = float(input("Digite o valor em reais:R$"))
 
 print('Valor em dólar:${}'.format(valor*5.43))

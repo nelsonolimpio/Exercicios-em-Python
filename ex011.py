516667a7acb997e592261081fe016d5b08ecacad
Nelson Waiss Urenha Olimpio <nelsonolimpio@outlook.com>
2022-04-21 21:53:42 -0300

    Calculadora pra uso de tinta na parede.

--- ex011.py
+++ ex011.py
@@ -1,5 +1,5 @@
-#Faça um programa que leia a largura e a altura de uma parede em metros. Calcule sua área e a quantidade
-#de tinta necessária para pintá-la. Sabendo que cada litro de tinta pinta uma área de 2m².
+'''Faça um programa que leia a largura e a altura de uma parede em metros. Calcule sua área e a quantidade
+de tinta necessária para pintá-la. Sabendo que cada litro de tinta pinta uma área de 2m².'''
 
 lar = float(input("Digite a largura:"))
 alt = float(input("Digite a altura:"))

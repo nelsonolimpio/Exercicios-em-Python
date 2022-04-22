8726428a429ae6db69049044435c86e9bd8cef43
Nelson Waiss Urenha Olimpio <nelsonolimpio@outlook.com>
2022-04-21 21:54:58 -0300

    Calculo de 5% no valor de um produto.

--- ex012.py
+++ ex012.py
@@ -1,4 +1,4 @@
-#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto
+'''Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto'''
 
 valor = float(input('Digite o valor do produto:'))
 

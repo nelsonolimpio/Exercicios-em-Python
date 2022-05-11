d7d436e6076fe27ec71e8f5163a674b0b5d69509
Nelson Waiss Urenha Olimpio <nelsonolimpio@outlook.com>
2022-05-11 20:29:36 -0300

    Automação-Abrindo o Google Chrome automaticamente.

---
+++ Abrir o Google Chrome automatico.py
@@ -0,0 +1,9 @@
+import pyautogui
+import pyperclip
+import time
+pyautogui.PAUSE = 1 #Entre cada código, haverá uma pequena pause de 1 segundo
+#Abrindo o navegador:
+#Abaixo vamos dar alguns ordens ao nosso teclado.
+pyautogui.press("win")#Pressionar a tecla Windowns
+pyautogui.write("Google Chrome")#Digitar Google Chrome
+pyautogui.press("enter")#Pressionar a tecla enter
